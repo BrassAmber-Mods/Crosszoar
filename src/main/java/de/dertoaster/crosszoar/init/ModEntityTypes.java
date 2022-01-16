4bb277b5b7fb90d9719f910b0a2ced92c24a9e0b
DerToaster98 <seiler.oli@web.de>
2022-01-16 13:07:26 +0100

    register items and projectiles

--- src/main/java/de/dertoaster/crosszoar/init/ModEntityTypes.java
+++ src/main/java/de/dertoaster/crosszoar/init/ModEntityTypes.java
@@ -1,12 +1,19 @@
 package de.dertoaster.crosszoar.init;
 
 import de.dertoaster.crosszoar.CrosszoarMod;
+import de.dertoaster.crosszoar.entity.projectile.ProjectileTortoiseBolt;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModEntityTypes {
 	
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, CrosszoarMod.MODID);
+	
+	public static final RegistryObject<EntityType<ProjectileTortoiseBolt>> BOLT_TORTOISE = ENTITY_TYPES.register("bolt_tortoise", () -> EntityType.Builder.<ProjectileTortoiseBolt>of(ProjectileTortoiseBolt::new, MobCategory.MISC).sized(0.5F, 0.5F).clientTrackingRange(4)
+			.updateInterval(20).build(new ResourceLocation(CrosszoarMod.MODID, "bolt_tortoise").toString()));
 
 }
--- src/main/java/de/dertoaster/crosszoar/init/ModItems.java
+++ src/main/java/de/dertoaster/crosszoar/init/ModItems.java
@@ -1,11 +1,27 @@
 package de.dertoaster.crosszoar.init;
 
+import de.dertoaster.crosszoar.CrosszoarMod;
+import de.dertoaster.crosszoar.item.ItemTortoiseBolt;
+import de.dertoaster.crosszoar.item.ItemTortoiseCrossbow;
+import milamber.brass.bezoar.util.BezoarTiers;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
 
 	public static void registerToEventBus(IEventBus modbus) {
-		
+		ITEMS.register(modbus);
 	}
+	
+	static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CrosszoarMod.MODID);
 
+	
+	public static final RegistryObject<Item>  ITEM_BOLT_TORTOSE = ITEMS.register("bolt_tortoise", () -> new ItemTortoiseBolt((new Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(64)), BezoarTiers.TORTOISE));
+	
+	public static final RegistryObject<Item> ITEM_CROSSBOW_TORTOISE = ITEMS.register("crossbow_tortoise", () -> (new ItemTortoiseCrossbow(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT).durability(2048).fireResistant())));
 }
